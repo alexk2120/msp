54e6f66063dd363be9e4936df255e1d879561abd
Alexander Khatamov <90288531+alexk2120@users.noreply.github.com>
2023-06-12 23:55:52 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function App() {
+  // import React from 'react';
+
+  // function App() {
+    return (
+      <div className="nabbar-options" >
+        <h1 className="title valign-text-middle" >
+          Products
+        </h1>
+        <div className="about valign-text-middle" >
+          About
+        </div>
+        <div className="place valign-text-middle" >
+          Contact
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
+  
+   
+   
+  
+   
+
